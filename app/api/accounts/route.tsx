329a2d1fd4cd6bd4e349d86f01e04c74f01356ad
hassanuahmad <ahmad12p@uwindsor.ca>
2024-02-18 09:24:36 -0500

    added conditional rendering in navbar and home page

--- app/api/accounts/route.tsx
+++ app/api/accounts/route.tsx
@@ -3,25 +3,21 @@ import prisma from "@/prisma/client";
 
 // request to create a new account
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+    const body = await request.json();
 
-  const account = await prisma.account.findUnique({
-    where: {
-      clerkId: body.clerkId,
-    },
-  });
+    const account = await prisma.account.findUnique({
+        where: {
+            clerkId: body.clerkId,
+        },
+    });
 
-  if (account)
-    return NextResponse.json(
-      { error: "Account already exists" },
-      { status: 400 }
-    );
+    if (account) return NextResponse.json(account, { status: 201 });
 
-  const newAccount = await prisma.account.create({
-    data: {
-      clerkId: body.clerkId,
-    },
-  });
+    const newAccount = await prisma.account.create({
+        data: {
+            clerkId: body.clerkId,
+        },
+    });
 
-  return NextResponse.json(newAccount, { status: 201 });
+    return NextResponse.json(newAccount, { status: 201 });
 }
